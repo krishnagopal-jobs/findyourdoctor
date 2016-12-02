c3c2a8547d4896784f038a5c3639afadff0915b3
kisna <kisna@Kisna-Suji>
2016-12-02 01:20:21 -0500

    Adding the first query

--- FindYourDoctor/src/main/java/edu/hood/cs/it530/findyourdoctor/HelloController.java
+++ FindYourDoctor/src/main/java/edu/hood/cs/it530/findyourdoctor/HelloController.java
@@ -1,24 +1,26 @@
 package edu.hood.cs.it530.findyourdoctor;
 
+import java.sql.SQLException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import edu.hood.cs.it530.findyourdoctor.doctordetails.DoctorInsertionModule;
+import edu.hood.cs.it530.findyourdoctor.searchphysician.SearchPhysicianDao;
 
 @Component
 @Path("/hello")
 public class HelloController {
-	
-	@Autowired
-	private DoctorInsertionModule doctorInsertionModule;
-	
-	@GET
-	public String index() {
-		//doctorInsertionModule.getDetails();
-		return "Greetings from Spring Boot! \n";
-	}
+
+    @Autowired
+    private SearchPhysicianDao searchPhysicianDao;
+
+    @GET
+    public String index() throws SQLException {
+        searchPhysicianDao.getPhysicians(20147, 1, "firstName", "lastName");
+        return "Greetings from Spring Boot! \n";
+    }
 
 }
